bdb88f8ff01643c720e2094800ad259b3c713506
hoho2b <38067903+chAwater@users.noreply.github.com>
2022-03-31 00:04:14 +0800

    Update 01.two-sum.py

--- 01.two-sum.py
+++ 01.two-sum.py
@@ -6,9 +6,9 @@ from typing import List
 class Solution1:
     '''
     Date: 2022.03.30
-    Pass/Error/Bug: 1/3/3
-    执行用时：40.0 ms, 在所有 Python3 提交中击败了 72.05% 的用户
-    内存消耗：16.8 MB, 在所有 Python3 提交中击败了  5.00% 的用户
+    Pass/Error/Bug: 2/3/5
+    执行用时：28.0 ms, 在所有 Python3 提交中击败了 98.42% 的用户
+    内存消耗：16.1 MB, 在所有 Python3 提交中击败了 17.90% 的用户
     '''
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
@@ -16,17 +16,23 @@ class Solution1:
         v_dict = {}
         for i, v in enumerate(nums):
             if v in v_dict:
-                v_dict[v].append(i)
+                last = v_dict[v]
+                if type(last) == list:
+                    v_dict[v] += [i]
+                else:
+                    v_dict[v] = [last, i]
             else:
-                v_dict[v] = [i]
+                v_dict[v] = i
 
         for v1, idxs1 in v_dict.items():
             v2 = target - v1
             if v1 == v2:
-                if len(idxs1) >= 2:
+                if (type(idxs1) == list) and (len(idxs1) >= 2):
                     return idxs1[0], idxs1[1]
             elif v2 in v_dict:
                 idxs2 = v_dict[v2]
-                return idxs1[0], idxs2[0]
+                idx1 = idxs1[0] if type(idxs1) == list else idxs1
+                idx2 = idxs2[0] if type(idxs2) == list else idxs2
+                return idx1, idx2
             else:
                 continue
