f94cbe3abe58ebc3c0f422a1a2e08f391f639f79
chenhe <38067903+chAwater@users.noreply.github.com>
2022-04-01 18:09:08 +0800

    Update 03.longest-substring-without-repeating-characters.py Solution1, move two index to save time

--- 03.longest-substring-without-repeating-characters.py
+++ 03.longest-substring-without-repeating-characters.py
@@ -3,24 +3,24 @@
 class Solution1:
     '''
     Date: 2022.04.01
-    Pass/Error/Bug: 1/0/0
-    执行用时： 568 ms, 在所有 Python3 提交中击败了  8.06% 的用户
-    内存消耗：15.2 MB, 在所有 Python3 提交中击败了 29.34% 的用户
+    Pass/Error/Bug: 2/1/0
+    执行用时： 444 ms, 在所有 Python3 提交中击败了 10.46% 的用户
+    内存消耗：14.9 MB, 在所有 Python3 提交中击败了 98.56% 的用户
     '''
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_sub_l = 0
-        for idx in range(len(s)):
-            sub_s = s[idx]
-            sub_i = 0
-            while True:
-                sub_next = s[idx+sub_i+1:idx+sub_i+2]
-                if sub_next in sub_s:
-                    break
-                else:
-                    sub_s += sub_next
-                    sub_i += 1
+        i_start = 0
+        i_end = 1
+        max_l = len(s)
 
-            if max_sub_l <= len(sub_s):
-                max_sub_l = len(sub_s)
-
-        return max_sub_l
+        while True:
+            if i_end > max_l:
+                return max_sub_l
+            sub_s = s[i_start:i_end]
+            sub_l = i_end - i_start
+            if len(dict(zip(sub_s, [1]*sub_l))) == sub_l:
+                if max_sub_l < sub_l:
+                    max_sub_l = sub_l
+                i_end += 1
+            else:
+                i_start += 1
