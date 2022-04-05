1271fa29fc208aefbe0fd35740d86300004fe915
chenhe <38067903+chAwater@users.noreply.github.com>
2022-04-05 12:37:33 +0800

    Add Solution2

--- 07.reverse-integer.py
+++ 07.reverse-integer.py
@@ -14,3 +14,43 @@ class Solution1:
             return o
         else:
             return 0
+
+
+class Solution2:
+    '''
+    Date: 2022.04.05
+    Pass/Error/Bug: 1/0/0
+    执行用时：  36 ms, 在所有 Python3 提交中击败了 76.48% 的用户
+    内存消耗：15.0 MB, 在所有 Python3 提交中击败了 18.03% 的用户
+    '''
+    def reverse(self, x: int) -> int:
+        if x == 0 or x == -2**31:
+            return 0
+
+        if x > 0:
+            mtag = 1
+        else:
+            mtag = -1
+            x = abs(x)
+
+        o = 0
+        i = 0
+
+        maxint = 2**31
+
+        while x != 0:
+            if (i >= 8):
+                if o > maxint // 10:
+                    return 0
+                if o == maxint // 10:
+                    if x % 10 > maxint % 10:
+                        return 0
+                    if x % 10 == maxint % 10:
+                        if mtag == 1:
+                            return 0
+
+            o = o * 10 + x % 10
+            x = x // 10
+            i += 1
+
+        return mtag * o
