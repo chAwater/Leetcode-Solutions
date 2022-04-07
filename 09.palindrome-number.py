5ccf463abe3049ef52b76017cbb69c7d07c70cee
chenhe <38067903+chAwater@users.noreply.github.com>
2022-04-07 17:28:15 +0800

    Add Solution2

--- 09.palindrome-number.py
+++ 09.palindrome-number.py
@@ -18,3 +18,26 @@ class Solution1:
                     return False
 
             return True
+
+
+class Solution2:
+    '''
+    Date: 2022.04.07
+    Pass/Error/Bug: 1/0/0
+    执行用时：  76 ms, 在所有 Python3 提交中击败了 22.25% 的用户
+    内存消耗：14.8 MB, 在所有 Python3 提交中击败了 83.67% 的用户
+    '''
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            nlist = []
+            while x != 0:
+                nlist.append(x % 10)
+                x = x // 10
+
+            for i in range(0, len(nlist)//2):
+                if nlist[i] != nlist[-1*i-1]:
+                    return False
+
+            return True
