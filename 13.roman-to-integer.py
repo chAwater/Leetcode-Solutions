577df7e9606762a328e68207489412483d16948d
chenhe <38067903+chAwater@users.noreply.github.com>
2022-04-13 09:25:14 +0800

    Add Solution2

--- 13.roman-to-integer.py
+++ 13.roman-to-integer.py
@@ -28,3 +28,37 @@ class Solution1:
                 r += romandict[sub_s]
 
         return r
+
+
+class Solution2:
+    '''
+    Date: 2022.04.13
+    Pass/Error/Bug: 1/0/0
+    执行用时：  48 ms, 在所有 Python3 提交中击败了 74.47% 的用户
+    内存消耗：15.0 MB, 在所有 Python3 提交中击败了 51.09% 的用户
+    '''
+    def romanToInt(self, s: str) -> int:
+        s = s.replace('IV', '|4|')
+        s = s.replace('IX', '|9|')
+        s = s.replace('XL', '|40|')
+        s = s.replace('XC', '|90|')
+        s = s.replace('CD', '|400|')
+        s = s.replace('CM', '|900|')
+        s = s.replace('I', '|1|')
+        s = s.replace('V', '|5|')
+        s = s.replace('X', '|10|')
+        s = s.replace('L', '|50|')
+        s = s.replace('C', '|100|')
+        s = s.replace('D', '|500|')
+        s = s.replace('M', '|1000|')
+
+        r = 0
+        for i in s.split('|'):
+            if i != '':
+                r += int(i)
+
+        return r
+
+        # Slower
+        # r = [ int(i) for i in s.split('|') if i!='' ]
+        # return sum(r)
