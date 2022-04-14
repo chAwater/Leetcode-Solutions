d5e895bd47f62ada197ee39e0c2e77764c44da06
chenhe <38067903+chAwater@users.noreply.github.com>
2022-04-14 22:59:17 +0800

    Add Solution2

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -18,3 +18,22 @@ class Solution1:
             else:
                 break
         return r
+
+
+class Solution2:
+    '''
+    Date: 2022.04.14
+    Pass/Error/Bug: 1/0/0
+    执行用时：  40 ms, 在所有 Python3 提交中击败了 51.41% 的用户
+    内存消耗：15.2 MB, 在所有 Python3 提交中击败了 22.28% 的用户
+    '''
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs = sorted(strs)
+        a, b = strs[0], strs[-1]
+        r = ''
+        for idx, sub_s in enumerate(a):
+            if sub_s == b[idx]:
+                r += sub_s
+            else:
+                break
+        return r
