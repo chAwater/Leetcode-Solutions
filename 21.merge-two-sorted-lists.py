ba22340fb9825b16443b450cf4e1107591b2c336
chenhe <38067903+chAwater@users.noreply.github.com>
2022-04-22 21:20:41 +0800

    Create 21.merge-two-sorted-lists.py

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -10,7 +10,13 @@ class ListNode:
         self.next = next
 
 
-class Solution:
+class Solution1:
+    '''
+    Date: 2022.04.22
+    Pass/Error/Bug: 1/1/0
+    执行用时：  48 ms, 在所有 Python3 提交中击败了 15.45% 的用户
+    内存消耗：15.1 MB, 在所有 Python3 提交中击败了  5.29% 的用户
+    '''
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if list1 is None:
             return list2
@@ -19,11 +25,11 @@ class Solution:
 
         if list1.val <= list2.val:
             return ListNode(
-                val = list1.val,
-                next=ListNode(list2.val, next=self.mergeTwoLists(list1.next, list2.next))
+                val=list1.val,
+                next=self.mergeTwoLists(list1.next, list2)
             )
         else:
             return ListNode(
-                val = list2.val,
-                next=ListNode(list1.val, next=self.mergeTwoLists(list1.next, list2.next))
+                val=list2.val,
+                next=self.mergeTwoLists(list1, list2.next)
             )
