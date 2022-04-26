6e19741efc000af9dd7cd21e5348e03b26063370
chenhe <38067903+chAwater@users.noreply.github.com>
2022-04-26 21:45:31 +0800

    Fix Typo

--- 24.swap-nodes-in-pairs.py
+++ 24.swap-nodes-in-pairs.py
@@ -2,10 +2,10 @@
 
 
 # Definition for singly-linked list.
- class ListNode:
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution1:
