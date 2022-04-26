57560d73d553a4c1b4bc44339693d7908440e4c0
chenhe <38067903+chAwater@users.noreply.github.com>
2022-04-26 21:42:03 +0800

    [OPT] Update Solution1

--- 24.swap-nodes-in-pairs.py
+++ 24.swap-nodes-in-pairs.py
@@ -11,24 +11,18 @@
 class Solution1:
     '''
     Date: 2022.04.26
-    Pass/Error/Bug: 1/0/0
-    执行用时：  52 ms, 在所有 Python3 提交中击败了  6.02% 的用户
-    内存消耗：15.0 MB, 在所有 Python3 提交中击败了 25.93% 的用户
+    Pass/Error/Bug: 2/0/0
+    执行用时：  36 ms, 在所有 Python3 提交中击败了 71.34% 的用户
+    内存消耗：15.0 MB, 在所有 Python3 提交中击败了 18.49% 的用户
     '''
     def swapPairs(self, head: ListNode) -> ListNode:
 
-        a1 = head
-        if a1 is None:
-            return None
-
-        a2 = a1.next
-        if a2 is None:
-            return a1
-
-        a3 = a2.next
-
-        a2.next = a1
-        a1.next = self.swapPairs(a3)
+        if (head is None) or (head.next is None):
+            return head
+        else:
+            a1 = head
+            a2 = head.next
+            a1.next = self.swapPairs(a2.next)
+            a2.next = a1
 
         return a2
-
