40cb118df4224235bae128d95e429e96ed5f8d6e
chenhe <38067903+chAwater@users.noreply.github.com>
2022-05-08 10:46:49 +0800

    Add new Solution for O(log(n))

--- 33.search-in-rotated-sorted-array.py
+++ 33.search-in-rotated-sorted-array.py
@@ -52,6 +52,37 @@ class Solution3:
     '''
     Date: 2022.05.06
     Pass/Error/Bug: 1/0/0
+    执行用时：  48 ms, 在所有 Python3 提交中击败了  6.93% 的用户
+    内存消耗：15.3 MB, 在所有 Python3 提交中击败了 22.71% 的用户
+    '''
+    def search(self, nums: List[int], target: int) -> int:
+        ls = len(nums)
+
+        if ls == 1:
+            if target == nums[0]:
+                return 0
+            else:
+                return -1
+
+        a = self.search(nums[:ls // 2], target)
+        b = self.search(nums[ls // 2:], target)
+
+        if (a != -1):
+            if (b == -1):
+                return a
+            else:
+                return -1
+        else:
+            if (b != -1):
+                return ls // 2 + b
+            else:
+                return -1
+
+
+class Solution4:
+    '''
+    Date: 2022.05.06
+    Pass/Error/Bug: 1/0/0
     执行用时：  40 ms, 在所有 Python3 提交中击败了 45.81% 的用户
     内存消耗：15.2 MB, 在所有 Python3 提交中击败了 56.71% 的用户
     '''
