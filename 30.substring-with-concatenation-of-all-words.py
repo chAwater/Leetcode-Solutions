d95a14c4a1c7779f3f1d0adc0c6eb01b1f4e49c1
chenhe <38067903+chAwater@users.noreply.github.com>
2022-05-10 23:02:20 +0800

    [DEV] Ongoing

--- 30.substring-with-concatenation-of-all-words.py
+++ 30.substring-with-concatenation-of-all-words.py
@@ -6,6 +6,37 @@ from typing import List
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
 
+        total_s = ''.join(words)
+        window_l = len(total_s)
+        n_words = len(words)
+        l_word = window_l // n_words
+        rs = []
+
+        for window_i in range(len(s)-window_l+1):
+            sub_s = s[window_i : window_i+window_l]
+            wdict = dict(zip(words, [0]*n_words))
+            good_flag = 1
+            for word_i in range(0, window_l, l_word):
+                sub_w = sub_s[word_i:word_i+l_word]
+                if sub_w in wdict:
+                    if wdict[sub_w] == 0:
+                        wdict[sub_w] = 1
+                    else:
+                        good_flag = 0
+                        break
+                else:
+                    good_flag = 0
+                    break
+            if good_flag:
+                rs.append(window_i)
+
+        return rs
+
+
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+
         idict = list( map(lambda x: ''.join(x), zip(*words)) )
         idx_dict = 0
         idx_s = 0
