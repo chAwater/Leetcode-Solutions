83893235a43a6ded97a3812362202916d77bb1e3
hoho2b <38067903+chAwater@users.noreply.github.com>
2022-08-21 15:56:43 +0800

    Update and Add Solution4

--- 03.longest-substring-without-repeating-characters.py
+++ 03.longest-substring-without-repeating-characters.py
@@ -72,7 +72,7 @@ class Solution2:
 class Solution3:
     '''
     Date: 2022.08.20
-    Pass/Error/Bug: 1/2/0
+    Pass/Error/Bug: 2/2/0
     执行用时：  80 ms, 在所有 Python3 提交中击败了 39.43% 的用户
     内存消耗：15.0 MB, 在所有 Python3 提交中击败了 86.96% 的用户
     '''
@@ -88,7 +88,7 @@ class Solution3:
             next_s = s[i_end]
             max_sub_l = max(max_sub_l, len(sub_s))
             if next_s in sub_s:
-                i_start += 1
+                i_start += sub_s.index(next_s) + 1
             else:
                 i_end += 1
 
@@ -97,11 +97,43 @@ class Solution3:
         return max_sub_l
 
 
+class Solution4:
+    '''
+    Date: 2022.08.21
+    Pass/Error/Bug: 2/6/1
+    执行用时： 224 ms, 在所有 Python3 提交中击败了 14.87% 的用户
+    内存消耗：15.0 MB, 在所有 Python3 提交中击败了 75.79% 的用户
+    '''
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_sub_l = 0
+        max_l = len(s)
+        s_dict = {}
+        i_start = 0
+        i_end = 1
+
+        while i_end <= max_l:
+            sub_s = s[i_start:i_end]
+
+            if sub_s[-1] in s_dict:
+                i_start = s_dict[sub_s[-1]] + 1
+                s_dict = dict(zip(s[i_start:i_end], range(i_start, i_end)))
+            else:
+                s_dict[sub_s[-1]] = i_end - 1
+                max_sub_l = max(max_sub_l, len(sub_s))
+
+            i_end += 1
+
+        return max_sub_l
+
+
 def test(c):
     f = c.lengthOfLongestSubstring
     assert f('au') == 2, f('au')
     assert f("abcabcbb") == 3, f("abcabcbb")
     assert f('aba') == 2, f('aba')
+    assert f('dvdf') == 3, f('dvdf')
+    assert f('tmmzuxt') == 5, f('tmmzuxt')
+    assert f('bbtablud') == 6, f('bbtablud')
     assert f('') == 0, f('')
     assert f(' ') == 1, f(' ')
     print ('Pass:{}'.format(str(c.__class__)))
@@ -109,6 +141,10 @@ def test(c):
 
 c1 = Solution1()
 c2 = Solution2()
+c3 = Solution3()
+c4 = Solution4()
 
 test(c1)
 test(c2)
+test(c3)
+test(c4)
