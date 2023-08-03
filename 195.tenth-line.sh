9ad0637af598b1677284af14856f2545f8b3a53a
hoho2b <38067903+chAwater@users.noreply.github.com>
2023-08-04 00:15:04 +0800

    Create 195.tenth-line.sh

---
+++ 195.tenth-line.sh
@@ -0,0 +1,8 @@
+# https://leetcode.cn/problems/tenth-line/
+# Date: 2023.08.02
+# Pass/Error/Bug: 1/0/0
+# 执行用时：   0 ms, 击败 100.00% 使用 Bash 的用户
+# 内存消耗：3.39 MB, 击败  81.59% 使用 Bash 的用户
+
+# Read from the file file.txt and output the tenth line to stdout.
+sed -n '10p' file.txt
